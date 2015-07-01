8c830063128b33f64a4262e877440765bc6435c8
nicktouchette <touchette@gmail.com>
2015-07-01 18:27:51 -0400

    server edit

--- server.js
+++ server.js
@@ -103,6 +103,8 @@ var SampleApp = function() {
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
+            res.setHeader('Content-Type', 'text/css');
+            res.send(self.cache_get('main.css') );
         };
     };
 
