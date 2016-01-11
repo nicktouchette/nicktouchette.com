2bd9ad165973c412976f39335268ede19aa83ef0
Nick Touchette <touchette@gmail.com>
2016-01-10 19:17:08 -0500

    removed setTimeout in place of TimelineMax

--- public/js/app.js
+++ public/js/app.js
@@ -39,13 +39,13 @@ var typing = new TimelineMax({repeat: -1, yoyo: true, paused: true});
 typing.to(armLeftFront, 0.05, {rotation: 15})
       .to(armRightFront, 0.05, {rotation: -15});
 
-var drink = new TimelineMax({onComplete: function(){this.reverse(0).delay(1);}, paused: true});
-drink.to(armRight, 0.2, {rotation: 32})
-     .to(armRightFront, 0.2, {rotation: -45}, 0)
-     .to(armRightFront, 0.2, {rotation: "+=65"})
-     .to(can, 0.2, {y: "-=60", x: "+=8", rotation: "+=35"}, 0.2)
-     .to(armRight, 0.2, {rotation: "+=42,", scaleY: "-=0.2"})
-     .to(can, 0.2, {x: "+=10", y: "-=60", rotation: "+=20"}, 0.4);
+var drink = new TimelineMax({repeatDelay: 1, repeat: 1, yoyo:true, paused:true});
+drink.to(armRight, 0.2, {rotation: 32}, "start")
+     .to(armRightFront, 0.2, {rotation: -45}, "start")
+     .to(armRightFront, 0.2, {rotation: "+=65"}, "lift")
+     .to(can, 0.2, {y: "-=60", x: "+=8", rotation: "+=35"}, "lift")
+     .to(armRight, 0.2, {rotation: "+=42,", scaleY: "-=0.2"}, "liftmore")
+     .to(can, 0.2, {x: "+=10", y: "-=60", rotation: "+=20"}, "liftmore");
 
 function raiseHead() {
   head.setAttribute('src','./img/robot/robot-head-up.png');
@@ -102,7 +102,7 @@ function delay(what, duration) {
   setTimeout(what, duration);
 }
 
-/////////////Timeline Code
+///////////Timeline Code
 TweenMax.to(light, 3, { rotation: "-3", yoyo: true, repeat: -1, ease: Power1.easeInOut });
 
 TweenMax.to([torsoTop, torsoDown, head, armLeft, armRight], 2, { marginTop: 5, yoyo: true, repeat: -1, ease: Power1.easeInOut });
@@ -111,8 +111,6 @@ TweenMax.to(monitor, 0.2, { opacity: 0.95, yoyo: true, repeat: -1, ease: Bounce.
 
 say(thingsToSay[0], content, 30);
 say(thingsToSay[1], nav, 100, 3000);
-setInterval(temporary, 10000);
 
-function temporary() {
-  drink.play();
-}
+var coffeeBreak = new TimelineMax({repeat: -1});
+coffeeBreak.add(drink.play(), 10);
