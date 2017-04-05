103b8eff4de93f611d25c0eac5ac0d628e34d14f
Nick Touchette <touchette@gmail.com>
2017-04-05 16:09:27 -0400

    remove files for old project

--- source/src/app/index.route.js
+++ source/src/app/index.route.js
@@ -13,34 +13,20 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'vm'
-      })
-      .state('projects', {
-        parent: 'home',
-        url: 'projects',
-        controller: 'ProjectsController',
-        controllerAs: 'vm',
-        templateUrl: 'app/projects/projects.html'
-      })
-      .state('projects.detail', {
-        url: '/:id',
-        controller: 'ProjectsDetailController',
-        controllerAs: 'vm',
-        templateUrl: 'app/projects/projectsDetail.html'
-      })
-      .state('about', {
-        parent: 'home',
-        url: 'about',
-        templateUrl: 'app/content/about.html'
-      })
-      .state('contact', {
-        parent: 'home',
-        url: 'contact',
-        templateUrl: 'app/content/contact.html'
-      })
-      .state('styles', {
-        url: '/styles',
-        templateUrl: 'app/styles/styles.html'
       });
+      // .state('projects', {
+      //   parent: 'home',
+      //   url: 'projects',
+      //   controller: 'ProjectsController',
+      //   controllerAs: 'vm',
+      //   templateUrl: 'app/projects/projects.html'
+      // })
+      // .state('projects.detail', {
+      //   url: '/:id',
+      //   controller: 'ProjectsDetailController',
+      //   controllerAs: 'vm',
+      //   templateUrl: 'app/projects/projectsDetail.html'
+      // });
 
     $urlRouterProvider.otherwise('/');
   }
