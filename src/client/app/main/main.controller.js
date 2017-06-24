79cf0f416a11cb46f9cf42a048907902054db4b9
Nick Touchette <touchette@gmail.com>
2017-06-24 11:22:07 -0400

    added a bunch of stuff

--- src/client/app/main/main.controller.js
+++ src/client/app/main/main.controller.js
@@ -9,6 +9,13 @@
   function MainController() {
     var vm = this;
 
+    vm.contact = {
+      name: '',
+      company: '',
+      email: '',
+      message: ''
+    }
+
     vm.sections = [{
       title: 'Test',
       image: '../assets/images/building.jpg',
@@ -30,5 +37,10 @@
       body: 'Lorem ipsum dolor sit amet, an mazim noluisse suscipiantur his. Nisl legendos ex eam, errem numquam reprehendunt sed id, no dicam iracundia sed. Sit verear antiopam ad, his movet aliquid repudiare ei. Scaevola consequat prodesset te mel. Meliore feugait nam ut, te pri liber iuvaret corpora.'
     }]
 
+    vm.scrollTo = function(section) {
+      console.log('scrolling')
+      $(section)
+        .velocity("scroll", { duration: 1000, easing: "easeInOutCirc" });
+    }
   }
 })();
